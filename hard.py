5fa384a7af857ec6b5852b1cadd3be8d1cc0706e
tawichi <otai0811@gmail.com>
2022-04-05 23:57:17 -0700

    add soft

--- hard.py
+++ hard.py
@@ -5,14 +5,13 @@ import operator
 import matplotlib.pyplot as plt
 import math
 import random
-from numpy.random import *
 from scipy import special
 
 S_REG = 3  # レジスタ数(前後半共通)
 LENGTH = 259  # 符号長
-TEST = 1  # テスト回数
+TEST = 500  # テスト回数
 OUT_BITS = 2  # 後半組は3
-OUT_LEN = LENGTH * OUT_BITS #777
+OUT_LEN = LENGTH * OUT_BITS
 K = S_REG + 1  # 拘束長は4(前後半共通)
 STATE_NUM = 8  # 後半組は16
 
@@ -28,14 +27,11 @@ def awgn(SNRdB, size):
 def hamming(s1, s2):
     # ハミング距離計算
     return sum(map(operator.xor, s1, s2))
-  
-def distance(s1,s2):
-    return  sum(abs(s1 - s2))
 
 
 def convolutional_encoder(data, state):
     # 状態と入力から，次の状態を返す
-    # 状態遷移図の規則h性を活用
+    # 状態遷移図の規則性を活用
     state = (2 * state + data) % 8
     return state
 
@@ -45,34 +41,26 @@ def combinations_count(n, r):
 
 
 # 初期化
-tdata = rdata = np.zeros((TEST, LENGTH), dtype=float)
-tcode = rcode =  rayleigh =  np.zeros((TEST, OUT_LEN), dtype=float)
+tdata = rdata = np.zeros((TEST, LENGTH), dtype=int)
+tcode = rcode = np.zeros((TEST, OUT_LEN), dtype=int)
 state = 0
 snr_list = []
 ber_list = []
 nocode_ber_list = []
 p_b_list = []
 
-
-
-
-
-    
-
-
-
 # 各時間，各状態において，ハミング距離を記録する
 # h[状態][時刻]
-h = 10000 * np.ones((STATE_NUM, LENGTH + 1), dtype=float)
+h = 10000 * np.ones((STATE_NUM, LENGTH + 1), dtype=int)
 h[0][0] = 0
 ##各時間(260)，各状態(8)へのパス(2;どの状態からどの入力)を記録する
 # path[状態][時刻][[前状態,入力]]
-path = np.zeros((STATE_NUM, LENGTH, 2), dtype=float)
+path = np.zeros((STATE_NUM, LENGTH, 2), dtype=int)
 transmit = receive = np.zeros((TEST, OUT_LEN))
 nocode_transmit = nocode_receive = nocode_demo = np.zeros((TEST, LENGTH))
 # 状態と入力が決まると，出力が決まる3次元配列
 # output[状態][入力][出力]
-output = np.zeros((STATE_NUM, 2, OUT_BITS), dtype=float)
+output = np.zeros((STATE_NUM, 2, OUT_BITS), dtype=int)
 output[0, 0] = [0, 0]
 output[0, 1] = [1, 1]
 output[1, 0] = [1, 1]
@@ -104,25 +92,19 @@ file_path = "./test.csv"  # CSVの書き込みpath．任意で変えて．
 if __name__ == "__main__":
     # 表示
     print("# SNR BER:")
-    
-    
 
     # 伝送シミュレーション
-    for SNRdB in np.arange(0, 6.25, 0.25):
+    for SNRdB in reversed(np.arange(0, 10, 0.25)):
         # 送信データの生成
         tdata = np.random.randint(0, 2, (TEST, LENGTH - S_REG))  # 送信データをランダムのバイナリで生成
-        rdata = np.zeros((TEST, LENGTH), dtype=float)
+        rdata = np.zeros((TEST, LENGTH), dtype=int)
 
         # 終端ビット系列の付加h
-        end = np.zeros((TEST, S_REG), dtype=float)
+        end = np.zeros((TEST, S_REG), dtype=int)
         tdata = np.append(tdata, end, axis=1)
 
         # 畳み込み符号化
         for i in range(TEST):
-            for k in range(OUT_LEN):
-                test = (randn(1, 1) + 1j * randn(1, 1)) * 1 / np.sqrt(2)
-                
-                
             for j in range(LENGTH):
                 if j == 0:
                     state = 0
@@ -137,12 +119,12 @@ if __name__ == "__main__":
         nocode_transmit[tdata == 1] = 1
 
         # 伝送
-        receive = rayleigh * transmit + awgn(SNRdB, (TEST, OUT_LEN))
+        receive = transmit + awgn(SNRdB, (TEST, OUT_LEN))
         nocode_receive = nocode_transmit + awgn(SNRdB, (TEST, LENGTH))
 
         # BPSK復調
-        #rcode[receive < 0] = 0
-        #rcode[receive >= 0] = 1
+        rcode[receive < 0] = 0
+        rcode[receive >= 0] = 1
 
         nocode_demo[nocode_receive < 0] = 0
         nocode_demo[nocode_receive >= 0] = 1
@@ -157,22 +139,22 @@ if __name__ == "__main__":
 
                 if j == 0:
                     h[0][0] = 0
-                    h[0][1] = h[0][0] + distance(output[0][0], r_pair)
-                    h[1][1] = h[0][0] + distance(output[0][1], r_pair)
+                    h[0][1] = h[0][0] + hamming(output[0][0], r_pair)
+                    h[1][1] = h[0][0] + hamming(output[0][1], r_pair)
 
-                    # h[0][2] = h[0][1] + distance(output[0][0], r_pair)
-                    # h[1][2] = h[0][1] + distance(output[0][1], r_pair)
-                    # h[2][2] = h[1][1] + distance(output[1][0], r_pair)
-                    # h[3][2] = h[1][1] + distance(output[1][1], r_pair)
+                    # h[0][2] = h[0][1] + hamming(output[0][0], r_pair)
+                    # h[1][2] = h[0][1] + hamming(output[0][1], r_pair)
+                    # h[2][2] = h[1][1] + hamming(output[1][0], r_pair)
+                    # h[3][2] = h[1][1] + hamming(output[1][1], r_pair)
 
-                    # h[0][3] = h[0][2] + distance(output[0][0], r_pair)
-                    # h[1][3] = h[0][2] + distance(output[0][1], r_pair)
-                    # h[2][3] = h[1][2] + distance(output[1][0], r_pair)
-                    # h[3][3] = h[1][2] + distance(output[1][1], r_pair)
-                    # h[4][3] = h[2][2] + distance(output[2][0], r_pair)
-                    # h[5][3] = h[2][2] + distance(output[2][1], r_pair)
-                    # h[6][3] = h[3][3] + distance(output[3][0], r_pair)
-                    # h[7][3] = h[3][3] + distance(output[3][1], r_pair)
+                    # h[0][3] = h[0][2] + hamming(output[0][0], r_pair)
+                    # h[1][3] = h[0][2] + hamming(output[0][1], r_pair)
+                    # h[2][3] = h[1][2] + hamming(output[1][0], r_pair)
+                    # h[3][3] = h[1][2] + hamming(output[1][1], r_pair)
+                    # h[4][3] = h[2][2] + hamming(output[2][0], r_pair)
+                    # h[5][3] = h[2][2] + hamming(output[2][1], r_pair)
+                    # h[6][3] = h[3][3] + hamming(output[3][0], r_pair)
+                    # h[7][3] = h[3][3] + hamming(output[3][1], r_pair)
 
                 else:
 
@@ -180,7 +162,7 @@ if __name__ == "__main__":
 
                     # template
 
-                    # if (h[状態a][j-1]+distance(output[状態a][入力a],r_pair)) <(h[状態b][j-1] + distance(output[状態b][入力b],r_pair)):
+                    # if (h[状態a][j-1]+hamming(output[状態a][入力a],r_pair)) <(h[状態b][j-1] + hamming(output[状態b][入力b],r_pair)):
                     #     h[2][j] = 前者
                     #     path[2][j]  = [状態a,入力a]
 
@@ -190,104 +172,104 @@ if __name__ == "__main__":
 
                     # 状態0
                     # 左辺の方がパスメトリック小さい場合
-                    if (h[0][j - 1] + distance(output[0][0], r_pair)) < (
-                        h[4][j - 1] + distance(output[4][0], r_pair)
+                    if (h[0][j - 1] + hamming(output[0][0], r_pair)) < (
+                        h[4][j - 1] + hamming(output[4][0], r_pair)
                     ):
-                        h[0][j] = h[0][j - 1] + distance(
+                        h[0][j] = h[0][j - 1] + hamming(
                             output[0][0], r_pair
                         )  # ハミング距離更新．(状態0時刻jのハミング距離を求める)
                         path[0][j] = [0, 0]  # パスの記録(状態0からの入力0)
 
                     # 右辺の方がパスメトリック小さい場合
                     else:
-                        h[0][j] = h[4][j - 1] + distance(
+                        h[0][j] = h[4][j - 1] + hamming(
                             output[4][0], r_pair
                         )  # ハミング距離更新
                         path[0][j] = [4, 0]  # パスの記録，(状態4からの入力0)
 
                     # 状態1
-                    if (h[0][j - 1] + distance(output[0][1], r_pair)) < (
-                        h[4][j - 1] + distance(output[4][1], r_pair)
+                    if (h[0][j - 1] + hamming(output[0][1], r_pair)) < (
+                        h[4][j - 1] + hamming(output[4][1], r_pair)
                     ):  # 状態1時刻jのハミング距離を求める
-                        h[1][j] = h[0][j - 1] + distance(output[0][1], r_pair)
+                        h[1][j] = h[0][j - 1] + hamming(output[0][1], r_pair)
                         path[1][j] = [0, 1]  # 状態1に来るパスは，状態0からの入力1
 
                     else:
-                        h[1][j] = h[4][j - 1] + distance(
+                        h[1][j] = h[4][j - 1] + hamming(
                             output[4][1], r_pair
                         )  # 状態1時刻jのハミング距離を求める
                         path[1][j] = [4, 1]  # 状態1に来るパスは，状態4からの入力1
 
                     ##状態2
 
-                    if (h[1][j - 1] + distance(output[1][0], r_pair)) < (
-                        h[5][j - 1] + distance(output[5][0], r_pair)
+                    if (h[1][j - 1] + hamming(output[1][0], r_pair)) < (
+                        h[5][j - 1] + hamming(output[5][0], r_pair)
                     ):
-                        h[2][j] = h[1][j - 1] + distance(output[1][0], r_pair)
+                        h[2][j] = h[1][j - 1] + hamming(output[1][0], r_pair)
                         path[2][j] = [1, 0]
 
                     else:
-                        h[2][j] = h[5][j - 1] + distance(output[5][0], r_pair)
+                        h[2][j] = h[5][j - 1] + hamming(output[5][0], r_pair)
                         path[2][j] = [5, 0]
 
                     ##状態3
 
-                    if (h[1][j - 1] + distance(output[1][1], r_pair)) < (
-                        h[5][j - 1] + distance(output[5][1], r_pair)
+                    if (h[1][j - 1] + hamming(output[1][1], r_pair)) < (
+                        h[5][j - 1] + hamming(output[5][1], r_pair)
                     ):
-                        h[3][j] = h[1][j - 1] + distance(output[1][1], r_pair)
+                        h[3][j] = h[1][j - 1] + hamming(output[1][1], r_pair)
                         path[3][j] = [1, 1]
 
                     else:
-                        h[3][j] = h[5][j - 1] + distance(output[5][1], r_pair)
+                        h[3][j] = h[5][j - 1] + hamming(output[5][1], r_pair)
                         path[3][j] = [5, 1]
 
                     ##状態4
 
-                    if (h[2][j - 1] + distance(output[2][0], r_pair)) < (
-                        h[6][j - 1] + distance(output[6][0], r_pair)
+                    if (h[2][j - 1] + hamming(output[2][0], r_pair)) < (
+                        h[6][j - 1] + hamming(output[6][0], r_pair)
                     ):
-                        h[4][j] = h[2][j - 1] + distance(output[2][0], r_pair)
+                        h[4][j] = h[2][j - 1] + hamming(output[2][0], r_pair)
                         path[4][j] = [2, 0]
 
                     else:
-                        h[4][j] = h[6][j - 1] + distance(output[6][0], r_pair)
+                        h[4][j] = h[6][j - 1] + hamming(output[6][0], r_pair)
                         path[4][j] = [6, 0]
 
                     ##状態5
 
-                    if (h[2][j - 1] + distance(output[2][1], r_pair)) < (
-                        h[6][j - 1] + distance(output[6][1], r_pair)
+                    if (h[2][j - 1] + hamming(output[2][1], r_pair)) < (
+                        h[6][j - 1] + hamming(output[6][1], r_pair)
                     ):
-                        h[5][j] = h[2][j - 1] + distance(output[2][1], r_pair)
+                        h[5][j] = h[2][j - 1] + hamming(output[2][1], r_pair)
                         path[5][j] = [2, 1]
 
                     else:
-                        h[5][j] = h[6][j - 1] + distance(output[6][1], r_pair)
+                        h[5][j] = h[6][j - 1] + hamming(output[6][1], r_pair)
                         path[5][j] = [6, 1]
 
                     ##状態6
 
-                    if (h[3][j - 1] + distance(output[3][0], r_pair)) < (
-                        h[7][j - 1] + distance(output[7][0], r_pair)
+                    if (h[3][j - 1] + hamming(output[3][0], r_pair)) < (
+                        h[7][j - 1] + hamming(output[7][0], r_pair)
                     ):
-                        h[6][j] = h[3][j - 1] + distance(output[3][0], r_pair)
+                        h[6][j] = h[3][j - 1] + hamming(output[3][0], r_pair)
                         path[6][j] = [3, 0]
 
                     else:
-                        h[6][j] = h[7][j - 1] + distance(output[7][0], r_pair)
+                        h[6][j] = h[7][j - 1] + hamming(output[7][0], r_pair)
                         path[6][j] = [7, 0]
 
                     ##状態7
 
-                    if (h[3][j - 1] + distance(output[3][1], r_pair)) < (
-                        h[7][j - 1] + distance(output[7][1], r_pair)
+                    if (h[3][j - 1] + hamming(output[3][1], r_pair)) < (
+                        h[7][j - 1] + hamming(output[7][1], r_pair)
                     ):
-                        h[7][j] = h[3][j - 1] + distance(output[3][1], r_pair)
+                        h[7][j] = h[3][j - 1] + hamming(output[3][1], r_pair)
                         path[7][j] = [3, 1]
 
                     else:
-                        h[7][j] = h[7][j - 1] + distance(output[7][1], r_pair)
+                        h[7][j] = h[7][j - 1] + hamming(output[7][1], r_pair)
                         path[7][j] = [7, 1]
 
                 # path[0][0] = [0, 0]
@@ -334,7 +316,7 @@ if __name__ == "__main__":
 
         p = 1 / 2 * special.erfc(np.sqrt(1 / 2 * 10 ** (SNRdB / 10)))
 
-        for k in range(6, 14):  # 後半組はrange(10,18)
+        for k in range(6, 13):  # 後半組はrange(10,18)
             if k % 2 == 0:
                 for e in range(k // 2 + 1, k):
                     p_k[k] = combinations_count(k, e) * p ** (e) * (1 - p) ** (k - e)
